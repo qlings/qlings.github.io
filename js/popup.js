11a602a7d08c9da2e34413ce488798e97c78ef7b
QLing <liminadd@gmail.com>
2015-02-14 22:22:06 +0800

    a

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,7 @@ xmlhttp=new XMLHttpRequest();
 xmlhttp.open("GET",url,false);
 xmlhttp.send();
 xmlDoc=xmlhttp.responseXML;
-var x=xmlDoc.getElementsByTagName("category")[0];
+var x=xmlDoc..childNodes.getElementsByTagName("category")[0];
 var txt = x.getElementsByTagName("entry")[1].childNodes[3];
 alert(txt.textContent);
 function display()
